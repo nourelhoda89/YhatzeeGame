97f92dda4873258b184376265432aac22fbd26a6
nourelhoda89 <nfarhat6558@volmail.jalc.edu>
2017-11-09 23:32:48 -0600

    "still fixing Dice"

--- src/Dice.java
+++ src/Dice.java
@@ -1,20 +1,28 @@
 import java.util.Stack;
 
-public class Dice {
+public class Dice{
+
 	public final static int MAX_NUM_ROLLS=5;
 	private int dieFace;
-	Stack<Integer> dieStack= new Stack<>();
-	
-	public Stack<Integer> rollDice(){
+	private Stack<Integer> dieStack= new Stack<>();
+
+	private Stack<Integer> rollDie(){
+
 		for(int i=0;i<MAX_NUM_ROLLS; i++){
 			dieFace=(int) (Math.random() * 6 + 1);
 			dieStack.add(dieFace);
 		}
 		return dieStack;
 	}
-public void rollAgain(int nbOfRolls, boolean again) {
-	if (nbOfRolls<3) rollDice();
+	public Stack<Integer> getDieStack(){
+		dieStack=rollDie();
+		return this.dieStack;
+	}
+	public void rollAgain(int nbOfRolls, boolean again) {
+		if (nbOfRolls<3) getDieStack();
 
-}
-
+	}
+	public void frequency() {
+		
+	}
 }
---
+++ src/Player.java
@@ -0,0 +1,20 @@
+
+public class Player {
+	
+    private int score;
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+    public void addScore(int roundScore){
+        this.score += roundScore; 
+    
+    }
+    
+
+        
+}
