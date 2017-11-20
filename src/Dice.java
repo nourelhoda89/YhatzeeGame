ba743b034ed3a1b71ddd391dc45d3d0e84a5e0db
nourelhoda89 <nfarhat6558@volmail.jalc.edu>
2017-11-20 10:48:46 -0600

    "using generic collection"

--- src/Dice.java
+++ src/Dice.java
@@ -1,13 +1,17 @@
-import ArrayBag.ArrayBag;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class Dice{
 
 	private final static int NUM_Dice=5;
 	private int dieFace;
 	int currentScore;
-	private ArrayBag<Integer> dieBag;
-	private ArrayBag<Integer> freqStack;
-	private ArrayBag<String> availableCategories;
+	private List<Integer> dieList;
+	private List<Integer> freqStack;
+	private Set<String> availableCategories;
+	//private List<String> availableCategories;
 	private int freqOne;
 	private int freqTwo;
 	private int freqThree;
@@ -16,15 +20,15 @@ public class Dice{
 	private int freqSix;
 
 	public Dice() {
-		dieBag= new ArrayBag<>(5);
-		freqStack= new ArrayBag<>(6);
+		dieList=  new ArrayList<>();
+		freqStack=  new ArrayList<>();;
 		currentScore=0;
-		availableCategories= new ArrayBag<>(13);
+		availableCategories= new HashSet<>();
 		freqOne= freqTwo=freqThree=freqFour=freqFive=freqSix=0;
 
 	}
-	public ArrayBag<Integer> rollDie(){
-dieBag.clear();
+	public List<Integer> rollDie(){
+		dieList.clear();
 		for(int i=0;i<NUM_Dice; i++){
 			dieFace=(int) (Math.random() * 6 + 1);
 			if(dieFace==1)freqOne++;
@@ -33,13 +37,13 @@ dieBag.clear();
 			else if(dieFace==4)freqFour++;
 			else if(dieFace==5)freqFive++;
 			else if(dieFace==6)freqSix++;
-			dieBag.add(dieFace);
+			dieList.add(dieFace);
 		}
-		return dieBag;
+		return dieList;
 	}
 
 
-	public ArrayBag<Integer> getFrequencies() {
+	public List<Integer> getFrequencies() {
 		freqStack.add(freqOne);
 		freqStack.add(freqTwo);
 		freqStack.add(freqThree);
@@ -48,7 +52,7 @@ dieBag.clear();
 		freqStack.add(freqSix);
 		return freqStack;
 	}
-	public ArrayBag<String> getCategories(){
+	public Set<String> getCategories(){
 		availableCategories.add("1-US1");
 		availableCategories.add("2-US2");
 		availableCategories.add("3-US3");
@@ -71,29 +75,79 @@ dieBag.clear();
 			availableCategories.add("12-Yahtzee");
 		//availableCategories.add("14-Yahtzee Bonus");
 		availableCategories.add("13-Chance");
-		
+
 		return availableCategories;
 	}
-
 	public int getRoundScore(String selection) {
+		int freq=0;
 		//upper Section
-		if(selection=="1")
-			currentScore=1*dieBag.getFrequencyOf(1);
-		else if(selection=="2")
-			currentScore=2*dieBag.getFrequencyOf(2);
-		else if(selection=="3")
-			currentScore=3*dieBag.getFrequencyOf(3);
-		else if(selection=="4")
-			currentScore=3*dieBag.getFrequencyOf(4);
-		else if(selection=="5")
-			currentScore=3*dieBag.getFrequencyOf(5);
-		else if(selection=="6")
-			currentScore=3*dieBag.getFrequencyOf(6);
+		if(selection=="1") {
+			while(!dieList.isEmpty()) {
+				for(int i=0;i<dieList.size();i++) {
+					if(dieList.get(i) ==1) {
+						freq++;
+					}
+				}
+			}
+			currentScore=1*freq;
+		}
+		else if(selection=="2") {
+			while(!dieList.isEmpty()) {
+				for(int i=0;i<dieList.size();i++) {
+					if(dieList.get(i) ==2) {
+						freq++;
+					}
+				}
+			}
+			currentScore=2*freq;
+		}
+		else if(selection=="3") {
+			while(!dieList.isEmpty()) {
+				for(int i=0;i<dieList.size();i++) {
+					if(dieList.get(i) ==3) {
+						freq++;
+					}
+				}
+			}
+			currentScore=3*freq;
+		}
+		else if(selection=="4") {
+			while(!dieList.isEmpty()) {
+				for(int i=0;i<dieList.size();i++) {
+					if(dieList.get(i) ==4) {
+						freq++;
+					}
+				}
+			}
+			currentScore=4*freq;
+		}
+		else if(selection=="5") {
+			while(!dieList.isEmpty()) {
+				for(int i=0;i<dieList.size();i++) {
+					if(dieList.get(i) ==5) {
+						freq++;
+					}
+				}
+			}
+			currentScore=5*freq;
+		}
+		else if(selection=="6") {
+			while(!dieList.isEmpty()) {
+				for(int i=0;i<dieList.size();i++) {
+					if(dieList.get(i) ==6) {
+						freq++;
+					}
+				}
+			}
+			currentScore=6*freq;
+		}
 
 		//lower Section
 		else if(selection=="7"|| selection=="8") {
-			while(!dieBag.isEmpty()) {
-				currentScore=+dieBag.remove();
+			while(!dieList.isEmpty()) {
+				for(int i=0;i<dieList.size();i++) {
+					currentScore=+dieList.remove(i);
+				}	
 			}
 		}
 		else if (selection=="9")
@@ -107,15 +161,21 @@ dieBag.clear();
 			currentScore=50;
 
 		else if (selection=="13") {
-			while(!dieBag.isEmpty()) {
-				currentScore=+dieBag.remove();
+			while(!dieList.isEmpty()) {
+				for(int i=0;i<dieList.size();i++) {
+					currentScore=+dieList.remove(i);
+				}	
 			}
 		}
-		//yahtzee bonus
+		//Yahtzee bonus
 		else if (selection=="14")
 			currentScore=100;
 		return currentScore;
 	}
-
+public static void main(String[] arg) {
+	Dice a= new Dice();
+	a.getCategories();
+	
+}
 }
 
